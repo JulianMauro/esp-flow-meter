a12d4e7290cbf6412690609c430f5b0f00b39b20
Mateus Lucas <mateusglucas@hotmail.com>
2024-02-29 15:59:36 -0300

    add doxygen to wave

--- components/wave/include/wave_common.h
+++ components/wave/include/wave_common.h
@@ -5,6 +5,9 @@
 
 #define WAVE_HEADER_SIZE             (44)
 
+/**
+ * @brief Header information struct
+ */
 typedef struct{
     uint16_t n_channels;
     uint16_t sample_rate;
@@ -12,4 +15,10 @@ typedef struct{
     uint16_t bytes_per_sample;
 }wave_header_t;
 
+/**
+ * @brief Print header bytes to buffer
+ * @param header header information
+ * @param buffer buffer to receive header bytes
+ * @note - Buffer length should be at least WAVE_HEADER_SIZE
+ */
 void wave_print_header(const wave_header_t *header, uint8_t *buffer);
